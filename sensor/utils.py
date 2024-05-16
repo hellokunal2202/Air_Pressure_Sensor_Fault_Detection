75f3273de14e3d793faf6af0ee7868faa78a17cf
Kunal Mali <malikunal2202@gmail.com>
2024-05-16 17:26:29 +0530

    utils added

---
+++ sensor/utils.py
@@ -0,0 +1,17 @@
+from .config import mongo_client
+import pandas as pd
+import json
+import logging
+# from .exception import SensorException
+
+
+def dump_csv_file_to_mongodb_collection(file_path:str,database_name:str,collection_name:str)->None:
+    try:
+        #reading a csv file
+        df = pd.read_csv(file_path)
+        logging.info(f"Rows and Columns {df.shape} ")
+        df.reset_index(drop=True,inplace=True)
+        json_records = list(json.loads(df.T.to_json()).values())  # converting the dataframe to json
+        mongo_client[database_name][collection_name].insert_many(json_records)
+    except Exception as e:
+        raise e
