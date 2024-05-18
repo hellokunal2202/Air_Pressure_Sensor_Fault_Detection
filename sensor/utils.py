e282c2d7d3f3329725e0850e1c51143bfcbd8762
Kunal Mali <malikunal2202@gmail.com>
2024-05-18 22:26:31 +0530

    read and write yaml file added

--- sensor/utils.py
+++ sensor/utils.py
@@ -4,7 +4,7 @@ import json
 import logging
 from .exception import SensorException
 import os,sys
-
+import yaml
 
 def dump_csv_file_to_mongodb_collection(file_path:str,database_name:str,collection_name:str)->None:
     try:
@@ -26,3 +26,21 @@ def export_collection_as_dataframe(database_name:str,collection_name:str)->pd.Da
         return df
     except Exception as e:
         raise SensorException(e,sys)
+
+#fuction to write yaml file
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
+    
+#function to read yaml file
+def read_yaml_file(file_path):
+    try:
+        with open(file_path,"rb") as file_reader:
+            return yaml.safe_load(file_reader)
+    except Exception as e:
+        raise SensorException(e, sys)
