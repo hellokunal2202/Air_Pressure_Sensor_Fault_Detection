40c6e2b94629b770a8aa85a3bdafb4e5a2885b96
Kunal Mali <malikunal2202@gmail.com>
2024-05-19 11:36:52 +0530

    data validation function added

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,8 +1,9 @@
-from sensor.entity.config_entity import (DataIngestionConfig, TrainingPipelineConfig)
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.config_entity import (DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig)
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 import os,sys 
 
 class TrainingPipeline:
@@ -25,10 +26,25 @@ class TrainingPipeline:
             except Exception as e:
                 raise SensorException(e, sys)
 
-        def start(self):
+        
+
+        def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
             try:
-                data_ingestion_artifact = self.start_data_ingestion()
-            
+                data_validation_config = DataValidationConfig(
+                    training_pipeline_config=self.training_pipleine_config)
+                data_validation = DataValidation(data_validation_config=data_validation_config,
+                data_ingestion_artifact=data_ingestion_artifact)
+                
+                return data_validation.initiate_data_validation()
             except Exception as e:
                 raise SensorException(e, sys)
 
+        def start(self):
+            try:
+               
+               data_ingestion_artifact = self.start_data_ingestion()
+               data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+                
+            except Exception as e:
+                print(e)
+                raise SensorException(e, sys)
